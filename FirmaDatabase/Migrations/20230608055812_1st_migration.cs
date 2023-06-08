3646d5c4c48e57fb7025ade8a3128d9ba7746ae8
Kaspar <kaspar.olman97gmail.com>
2023-06-08 09:17:26 +0300

    DATABASE HOMEWORK

---
+++ FirmaDatabase/Migrations/20230608055812_1st_migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirmaDb.Migrations
+{
+    public partial class _1st_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
